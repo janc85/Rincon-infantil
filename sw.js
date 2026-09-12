3c9bb7c8abf6a53ee197d0e31fed59eb564c8ef3
janc85 <janc85@gmail.com>
2026-09-12 11:39:25 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,35 @@
+// Service Worker - Rincón Infantil de Actividades
+const CACHE_NAME = 'rincon-infantil-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/icon-maskable-512.png'
+];
+
+// Instalación: guardar todos los archivos en caché
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+// Activación: limpiar cachés viejas
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Estrategia: caché primero, red como respaldo (funciona sin internet)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
